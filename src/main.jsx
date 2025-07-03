43bf25f318da0917849884910aeff28053e83fb9
Damir Syzdykov <damir.syzdykov@indriver.com>
2025-07-04 03:25:34 +0500

    upd: returned bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '/reset.css'
-import '/styles.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
